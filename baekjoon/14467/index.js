994ae62c39d2bcaddb8742b40406333deffdb6ca
meldyssey <meldyssey15@gmail.com>
2025-02-07 23:31:40 +0900

    2025-02-07: 14467번 문제 풀이

--- baekjoon/14467/index.js
+++ baekjoon/14467/index.js
@@ -17,7 +17,25 @@ function solution(input) {
     // input 가공, 소배열 : 위치
 
     let inputLine = input.map((v) => v.trim().split(" "));
-    console.log(inputLine);
+    let inputLocation = {};
+    // console.log(inputLine);
+    for (i = 0; i < inputLine.length; i++) {
+        if (
+            inputLine[i][0] in inputLocation &&
+            inputLocation[inputLine[i][0]] == inputLine[i][1]
+        ) {
+            continue;
+        } else if (
+            inputLine[i][0] in inputLocation &&
+            inputLocation[inputLine[i][0]] != inputLine[i][1]
+        ) {
+            inputLocation[inputLine[i][0]] = inputLine[i][1];
+            result++;
+        } else {
+            inputLocation[inputLine[i][0]] = inputLine[i][1];
+        }
+    }
+    // console.log(inputLocation);
 
-    return result;
+    return console.log(result);
 }
