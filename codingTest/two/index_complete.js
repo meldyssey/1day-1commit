3befa4a0fce24866c7013136446e04da87fb55e9
meldyssey <meldyssey15@gmail.com>
2025-02-09 12:34:30 +0900

    2025-02-09: 풀었던 문제 복습

--- codingTest/two/index_complete.js
+++ codingTest/two/index_complete.js
@@ -8,24 +8,26 @@ const inputLine = input.map((v) => v.split(" ").map(Number));
 console.log(inputLine);
 
 let resultArr = Array.from({ length: num[0] }, (v, i) => i + 1);
-console.log(resultArr);
+// console.log(resultArr);
 
-for (let i = 0; i < resultArr.length - 1; i++) {
+for (let i = 0; i < resultArr.length; i++) {
     let pairs = [];
+
     for (let j = i + 1; j < resultArr.length; j++) {
         pairs.push([resultArr[i], resultArr[j]]);
     }
     console.log(pairs);
     let toRemove = new Set();
     for (let pair of pairs) {
-        for (let k of inputLine) {
-            if (pair[0] === k[0] && pair[1] === k[1]) {
+        for (let i of inputLine) {
+            if (pair[0] === i[0] && pair[1] === i[1]) {
                 toRemove.add(pair[1]);
             }
         }
     }
-    resultArr = resultArr.filter((v) => !toRemove.has(v));
+
+    resultArr = resultArr.filter((num) => !toRemove.has(num));
 }
-console.log(resultArr);
+console.log("Filtered resultArr:", resultArr);
 
 console.log(resultArr.length);
