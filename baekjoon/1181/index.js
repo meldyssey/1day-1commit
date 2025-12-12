e3d027550ab324fbb86db1f2f9fe96126fb319db
meldyssey <meldyssey15@gmail.com>
2025-12-12 13:35:57 +0900

    1181 문제풀이

---
+++ baekjoon/1181/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const currentDir = path.basename(__dirname);
+const filePath =
+  process.platform === `linux` ? `dev/stdin` : `${currentDir}/input.txt`;
+const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  // 중복 제거
+  let inputSet = new Set(input);
+  let inputArr = [...inputSet];
+
+  inputArr.sort((a, b) => {
+    if (a.length !== b.length) {
+      // 길이가 다르면 길이 오름차순
+      return a.length - b.length;
+    }
+    // 길이가 같으면 사전 순으로
+    return a.localeCompare(b);
+  });
+
+  console.log(inputArr);
+}
+solution(input);
